636fc38fc8daa0a97a73b6cb4ab279525e82fb71
Imane Allay <iallay@xpanxion.com>
2023-07-27 18:52:40 -0400

    new changes

---
+++ src/webparts/helloImane/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IHelloImaneWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'HelloImaneWebPartStrings' {
+  const strings: IHelloImaneWebPartStrings;
+  export = strings;
+}
---
+++ src/webparts/helloworld2/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IHelloworld2WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'Helloworld2WebPartStrings' {
+  const strings: IHelloworld2WebPartStrings;
+  export = strings;
+}
